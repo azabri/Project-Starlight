66767d3f6393039eb3839ab49d0471d88478e18b
azabri <azabri5th@gmail.com>
2020-11-12 13:19:27 +0100

    Update switch_evaluation.go

--- November/switch_evaluation.go
+++ November/switch_evaluation.go
@@ -16,5 +16,15 @@ import(
     fmt.Println("In two days")
    default:
     fmt.Println("Too far...!")
+    
+    t := time.Now()
+    switch {
+    case t.Hour() < 12:
+      fmt.Println("Good morning!")
+    case t.Hour() < 17:
+      fmt.Println("Good afternoon!")
+    default:
+      fmt.Println("Good evening!")
+    }
   }
  }
