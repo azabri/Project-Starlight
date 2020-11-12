510cb7662ec4940c4110036843602f64d9937454
azabri <azabri5th@gmail.com>
2020-11-12 13:12:05 +0100

    Create switch.go

---
+++ November/switch.go
@@ -0,0 +1,17 @@
+package main
+import (
+  "fmt"
+  "runtime"
+ )
+ 
+func main() {
+  fmt.Println("Go runs on")
+  switch os := runtime.GOOS; os {
+  case "darwin":
+    fmt.Println("OS X.")
+   case "linux":
+    fmt.Println("linux")
+   default:
+    fmt.Println("%s. \n", os)
+  }
+}
